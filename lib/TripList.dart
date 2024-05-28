c0e778087e546569e767e090e3698935892b6186
hongsh26 <swatch6264@gmail.com>
2024-05-29 03:00:12 +0900

    backup2

--- lib/TripList.dart
+++ lib/TripList.dart
@@ -3,6 +3,10 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:intl/intl.dart';
+import 'ShowTrip.dart';
+import 'WriteTrip.dart';
+
 class TripList extends StatefulWidget {
   final int userId;
   final int year;
@@ -43,27 +47,195 @@ class _TripListState extends State<TripList> {
     }
   }
 
+  String formatDate(String dateStr) {
+    // Parse the date string
+    DateTime date = DateTime.parse(dateStr);
+    // Format the date to only show month and day
+    return DateFormat('MM/dd').format(date);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Trip List'),
+        backgroundColor: Colors.transparent,
+        scrolledUnderElevation: 0,
+        title: const Text(' '),
       ),
-      body: ListView.builder(
-        itemCount: trips.length,
-        itemBuilder: (context, index) {
-          // Convert the image path to a File object
-          String imagePath = trips[index]['image_first'];
-          File imageFile = File(imagePath);
-
-          return ListTile(
-            leading: imagePath != null && imageFile.existsSync()
-                ? Image.file(imageFile)
-                : Icon(Icons.image),
-            title: Text(trips[index]['title']),
-            subtitle: Text('${trips[index]['country']} - ${trips[index]['start_date']} to ${trips[index]['end_date']}'),
-          );
-        },
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 30.0, bottom: 15.0),
+                    child: ShaderMask(
+                      shaderCallback: (bounds) => LinearGradient(
+                        colors: [Colors.blueAccent, Colors.black],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ).createShader(bounds),
+                      child: Text(
+                        'TRIMO',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 25),
+              height: 16,
+              child: ShaderMask(
+                shaderCallback: (bounds) => LinearGradient(
+                  colors: [Colors.blueAccent, Colors.black],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ).createShader(bounds),
+                child: Text(
+                  '>2024',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 25, bottom: 20),
+              child: Text(
+                '여행 일지',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Scrollbar(
+                child: ListView.builder(
+                  itemCount: trips.length,
+                  itemBuilder: (context, index) {
+                    String imagePath = trips[index]['image_first'];
+                    File imageFile = File(imagePath);
+                    final trip = trips[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ShowTrip(tripId: trip['travel_id']),
+                          ),
+                        );
+                      },
+                      child: Card(
+                        color: Colors.white,
+                        margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 18.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(3),
+                          side: BorderSide(color: Colors.black, width: 0.5),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: ListTile(
+                            contentPadding: EdgeInsets.zero,
+                            leading: imagePath != null && imageFile.existsSync()
+                                ? Image.file(imageFile)
+                                : Icon(Icons.image),
+                            title: Text(
+                              trip['title'] ?? '',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            subtitle: Text(
+                              '${trips[index]['country']} \n${formatDate(trips[index]['start_date'])} ~ ${formatDate(trips[index]['end_date'])}',
+                              style: TextStyle(fontSize: 12),
+                            ),
+                            trailing: PopupMenuButton(
+                              itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+                                PopupMenuItem(
+                                  child: Text('수정'),
+                                  value: 'edit',
+                                ),
+                                PopupMenuItem(
+                                  child: Text('삭제'),
+                                  value: 'delete',
+                                ),
+                              ],
+                              onSelected: (value) {
+                                if (value == 'edit') {
+                                  // 수정 로직 추가
+                                } else if (value == 'delete') {
+                                  // 삭제 로직 추가
+                                }
+                              },
+                            ),
+                            isThreeLine: true,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        side: BorderSide(color: Colors.indigo, width: 1),
+                        backgroundColor: Colors.white,
+                      ),
+                      onPressed: () {
+                        showModalBottomSheet<void>(
+                          context: context,
+                          isScrollControlled: true,
+                          backgroundColor: Colors.white,
+                          barrierColor: Colors.transparent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.vertical(
+                              top: Radius.circular(15.0),
+                            ),
+                            side: BorderSide(
+                              color: Colors.black,
+                              width: 1,
+                            ),
+                          ),
+                          builder: (BuildContext context) {
+                            return WriteTrip();
+                          },
+                        );
+                      },
+                      child: const Text(
+                        "새 일지 생성",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
